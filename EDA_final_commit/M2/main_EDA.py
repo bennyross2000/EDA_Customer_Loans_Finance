e7879b132e4431996920f74982590b7ecd093765
Benny <bennyross@live.co.uk>
2024-02-11 17:08:06 +0000

    Add EDA_final_commit to EDA_Final

---
+++ EDA_final_commit/M2/main_EDA.py
@@ -0,0 +1,21 @@
+from db_utils import RDSDatabaseConnector
+
+credentials = {
+    'RDS_HOST': 'eda-projects.cq2e8zno855e.eu-west-1.rds.amazonaws.com',
+    'RDS_PASSWORD': 'EDAloananalyst',
+    'RDS_USER': 'loansanalyst',
+    'RDS_DATABASE': 'payments',
+    'RDS_PORT': '5432'
+}
+
+def main():
+    rds_connector = RDSDatabaseConnector(credentials)
+
+    data_frame = rds_connector.extract_data_to_dataframe()
+
+    file_path = 'C:/Users/admin/EDA - Customer Loans/loan_payments.csv'
+
+    rds_connector.save_to_csv(data_frame, file_path)
+
+if __name__ == "__main__":
+    main()
